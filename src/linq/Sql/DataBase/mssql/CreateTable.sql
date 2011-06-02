4d98f4305278a59ab97984e2bcc60f3444a5b7e0
zhli <zhli@localhost>
2011-06-02 13:53:30 +0800

    http://jira.txtek.com/browse/KISSPROJECT-61

--- src/linq/Sql/DataBase/mssql/CreateTable.sql
+++ src/linq/Sql/DataBase/mssql/CreateTable.sql
@@ -1,5 +1,5 @@
-﻿IF Not EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[#ENTITY#]') AND type in (N'U'))
-CREATE TABLE [dbo].[#ENTITY#]
+﻿IF Not EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[#ENTITY#]') AND type in (N'U'))
+CREATE TABLE [#ENTITY#]
 (
 	#PARAMS#
 )ON [PRIMARY];
