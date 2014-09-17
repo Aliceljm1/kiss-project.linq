c1f0f613689e06c95bb5e4a5ad1be0587930573d
zhli <zhli@localhost>
2014-09-17 20:46:21 +0800

    ??nuget????

--- src/linq/Properties/AssemblyInfo.cs
+++ src/linq/Properties/AssemblyInfo.cs
@@ -1,3 +1,10 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Kiss.Linq")]
+
+[assembly: AssemblyCompany("TXTEK")]
+[assembly: AssemblyCopyright("© TXTEK.CN - http://kiss.txtek.cn/")]
+[assembly: AssemblyProduct("KISS Project")]
+
+[assembly: AssemblyVersion("4.6.*")]
+[assembly: AssemblyFileVersion("4.6.20131129.0")]
